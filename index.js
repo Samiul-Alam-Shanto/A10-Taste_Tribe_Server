72672bb911da7add12343325ff002c7fde300865
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2025-11-12 16:23:24 +0600

    added the delete favorite api and trying to deploy it

--- index.js
+++ index.js
@@ -121,10 +121,17 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    app.delete("/favorite-reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await favoriteReviewsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // await client.close();
   }
