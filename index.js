77e2adf514f274411bda58aafcc63498c3743903
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-01-02 18:43:35 +0600

    working again on this project to improve it and now added some routes for the users collection and also added the verify admin api

--- index.js
+++ index.js
@@ -4,9 +4,51 @@ const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
+const admin = require("firebase-admin");
+
+let serviceAccount;
+try {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+  console.log("Firebase Admin Initialized Successfully");
+} catch (error) {
+  console.error("Failed to parse FIREBASE_SERVICE_ACCOUNT.");
+}
 
 app.use(cors());
 app.use(express.json());
+
+//middleware
+
+const verifyFirebaseToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+
+    req.token_email = decoded.email;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+};
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.token_email;
+
+  const query = { email: email };
+  const user = await usersCollection.findOne(query);
+
+  if (user?.role !== "admin") {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  next();
+};
 
 app.get("/", (req, res) => {
   res.send("Taste tribe Server is Running");
@@ -30,10 +72,71 @@ async function run() {
     const db = client.db("taste_tribe_DB");
     const reviewCollection = db.collection("reviews");
     const favoriteReviewsCollection = db.collection("Favorite_Reviews");
+    const usersCollection = db.collection("users");
+
+    //users api's
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      // Prevent duplicate users
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists", insertedId: null });
+      }
+      const result = await usersCollection.insertOne({ ...user, role: "user" });
+      res.send(result);
+    });
+
+    app.get("/users", verifyFirebaseToken, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users/:email", verifyFirebaseToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.token_email !== email) {
+        return res
+          .status(403)
+          .send({ message: "Forbidden: You can only access your own data." });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send(user);
+    });
+
+    app.patch(
+      "/users/admin/:id",
+      verifyFirebaseToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = { $set: { role: "admin" } };
+        const result = await usersCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
+
+    // DELETE a user (admin)
+    app.delete(
+      "/users/:id",
+      verifyFirebaseToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+      }
+    );
 
     // review api's
 
-    app.post("/reviews", async (req, res) => {
+    app.post("/reviews", verifyFirebaseToken, async (req, res) => {
       const newReview = req.body;
       newReview.rating = parseInt(newReview.rating, 10);
       newReview.postedDate = new Date();
@@ -57,25 +160,28 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/reviews/:id", async (req, res) => {
+    app.get("/reviews/:id", verifyFirebaseToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await reviewCollection.findOne(query);
       res.send(result);
     });
 
-    app.get("/my-reviews", async (req, res) => {
+    app.get("/my-reviews", verifyFirebaseToken, async (req, res) => {
       const email = req.query.email;
       const query = {};
       if (email) {
         query.reviewerEmail = email;
+        if (email !== req.token_email) {
+          return res.status(403).send({ message: "Forbidden Access" });
+        }
       }
       const cursor = reviewCollection.find(query).sort({ postedDate: -1 });
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.patch("/reviews/:id", async (req, res) => {
+    app.patch("/reviews/:id", verifyFirebaseToken, async (req, res) => {
       const id = req.params.id;
       const updateReview = req.body;
       const query = { _id: new ObjectId(id) };
@@ -84,7 +190,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/reviews/:id", async (req, res) => {
+    app.delete("/reviews/:id", verifyFirebaseToken, async (req, res) => {
       const id = req.params.id;
       const reviewQuery = { _id: new ObjectId(id) };
       const favReviewQuery = { reviewId: id };
@@ -100,7 +206,7 @@ async function run() {
 
     // Favorite Review APi's
 
-    app.post("/favorite-reviews", async (req, res) => {
+    app.post("/favorite-reviews", verifyFirebaseToken, async (req, res) => {
       const newFavoriteReview = req.body;
       const { userEmail, reviewId } = newFavoriteReview;
       const exist = await favoriteReviewsCollection.findOne({
@@ -115,7 +221,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/my-favorite-reviews", async (req, res) => {
+    app.get("/my-favorite-reviews", verifyFirebaseToken, async (req, res) => {
       const email = req.query.email;
       const query = {};
       if (email) {
@@ -128,12 +234,16 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/favorite-reviews/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await favoriteReviewsCollection.deleteOne(query);
-      res.send(result);
-    });
+    app.delete(
+      "/favorite-reviews/:id",
+      verifyFirebaseToken,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await favoriteReviewsCollection.deleteOne(query);
+        res.send(result);
+      }
+    );
 
     // await client.db("admin").command({ ping: 1 });
     // console.log(
